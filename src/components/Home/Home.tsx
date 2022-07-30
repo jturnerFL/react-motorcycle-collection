b301164499e7db5d7709d5cccdfbb49b205c4772
Jim Turner <jaxbeachcft@gmail.com>
2022-07-30 08:22:56 -0400

    commit2

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-// import {Navbar} from '../../components/Navbar';
+import {Navbar} from '../../components/Navbar';
 import { Link } from 'react-router-dom';
 
 
@@ -11,7 +11,8 @@ interface Props {
 
 const useStyles = makeStyles({
     background: {
-        backgroundImage: `linear-gradient( 134.6deg,  rgba(201,37,107,1) 15.4%, rgba(116,16,124,1) 74.7% )`,
+        backgroundImage: `linear-gradient(62deg, #682407 0%, #F7CE68 100%)`,
+
         width: '100%',
         height: '100%',
         backgroundPosition: 'center',
@@ -40,7 +41,7 @@ export const Home = ( props: Props) => {
 
     return (
      <>
-        {/*<Navbar />*/}
+        <Navbar />
         <div className={`${classes.background}`}>
             <div className={classes.main_text}>
                 <h1>{props.title}</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { FirebaseAppProvider, AuthCheck } from 'reactfire';
 import { Home } from './components';
+import './style.css';
+// import { firebaseConfig } from './firebaseConfig';
+// import 'firebase/auth';
+// import { Provider } from 'react-reudx';
+// import { store } from './redux/store';
+
 
 let myTitle = "Motorcycle Inventory"
 
@@ -9,8 +17,27 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Home title={myTitle} />
-  </React.StrictMode>
+
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Home title={myTitle} />
+        </Route>
+        <Route path='/inventory'>
+          <Inventory></Inventory>
+        </Route>
+        <Route path='/contact'>
+          <Contact></Contact>
+        </Route>
+        <Route path='/about'>
+          <About></About>
+        </Route>
+
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  
 );
 
 
